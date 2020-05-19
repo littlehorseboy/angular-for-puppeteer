1ca5cf89e5dbd255259596703aa74eddea1083f5
littlehorseboy <littlehorseboy@gmail.com>
2020-05-19 23:10:13 +0800

    feat(): InputTextModule p-grid p-col 好像有點不好用

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
@@ -11,6 +12,7 @@ import { LoginComponent } from './login.component';
   imports: [
     CommonModule,
     FormsModule,
+    InputTextModule,
     LoginRoutingModule
   ],
   exports: [
